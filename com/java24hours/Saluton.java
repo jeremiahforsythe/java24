7fb4546bd62b16717e90a5fc9702166a5ad8f2f6
jeremiah forsythe <jeremiah.forsythe@hotmail.com>
2023-02-22 00:04:37 -0600

    first commit

---
+++ com/java24hours/Saluton.java
@@ -0,0 +1,12 @@
+package com.java24hours;
+
+
+ class Saluton {
+
+    public static void main(String[] args) {
+        String greeting = "Saluton Mundo!";
+        System.out.println(greeting);
+    }
+
+   
+}
